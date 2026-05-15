f3d70e39ef35e807dcfa941f4d1e564b54fd16a2
Leandro Reis <leohbr@gmail.com>
2026-05-15 10:31:02 -0300

    update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,17 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   allowCypressEnv: false,
+  video: true,
+  screenshotOnRunFailure: true,
 
   e2e: {
+    baseUrl: 'https://front.serverest.dev',
+
+    env: {
+      apiUrl: 'https://serverest.dev'
+    },
+
     setupNodeEvents(on, config) {
-      // implement node event listeners here
     },
   },
 });
