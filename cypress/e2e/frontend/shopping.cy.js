74408e7b2d1da5c55d7e7947b98b0cae987a9436
Leandro Reis <leohbr@gmail.com>
2026-05-18 16:21:39 -0300

    refactor(e2e): decouple UI logic from pure commands and stabilize shopping scenarios

--- cypress/e2e/frontend/shopping.cy.js
+++ cypress/e2e/frontend/shopping.cy.js
@@ -1,56 +1,49 @@
-import HomePage from '../../support/pages/HomePage'
-import ShoppingCartPage from '../../support/pages/ShoppingCartPage'
+import { faker } from '@faker-js/faker'
+import HomePage from '../../pages/HomePage'
+import ShoppingCartPage from '../../pages/ShoppingCartPage'
+import LoginPage from '../../pages/LoginPage'
 
 describe('Shopping List Flow', () => {
 
-  it('Should add products to shopping list successfully', () => {
-    const productName = 'Logitech MX Vertical'
-    cy.createFinalUser()
-      .then(({ user }) => {
-        cy.login(
-          user.email,
-          user.password
-        )
+  let adminUser
+  let finalUser
+  let productName
 
-        HomePage.validateLoginSuccess()
-        HomePage.addProductToList(productName)
-        HomePage.accessShoppingList()
-        ShoppingCartPage.validateProductInList(productName)
-      })
+  beforeEach(() => {
+    productName = `${faker.commerce.product()} ${Date.now()}`
+
+    cy.createAdminUser().then(({ user }) => {
+      adminUser = user
+      cy.createProductViaAPI(adminUser.email, adminUser.password, productName)
+    })
+
+    cy.createFinalUser().then(({ user }) => {
+      finalUser = user
+    })
+  })
+
+  it('Should add products to shopping list successfully', () => {
+    LoginPage.login(finalUser.email, finalUser.password)
+    HomePage.validateLoginSuccess()
+    HomePage.addProductToList(productName)
+    HomePage.accessShoppingList()
+    ShoppingCartPage.validateProductInList(productName)
   })
 
   it('Should clear shopping list successfully', () => {
-    const productName = 'Logitech MX Vertical'
-    cy.createFinalUser()
-      .then(({ user }) => {
-        cy.login(
-          user.email,
-          user.password
-        )
-        HomePage.validateLoginSuccess()
-        HomePage.addProductToList(productName)
-        HomePage.accessShoppingList()
-        ShoppingCartPage.validateProductInList(productName)
-        ShoppingCartPage.clearShoppingList()
-        ShoppingCartPage.validateEmptyList(productName)
-      })
+    LoginPage.login(finalUser.email, finalUser.password)
+    HomePage.validateLoginSuccess()
+    HomePage.addProductToList(productName)
+    HomePage.accessShoppingList()
+    ShoppingCartPage.validateProductInList(productName)
+    ShoppingCartPage.clearShoppingList()
+    ShoppingCartPage.validateEmptyList(productName)
   })
 
-  it('Full flow: create product via API, add from frontend and validate persistence via API', () => {
-    const productName = 'Test Product API'
-    let adminUser
-
-    cy.createAdminUser()
-      .then(({ user }) => {
-        adminUser = user
-        cy.createProductViaAPI(adminUser.email, adminUser.password, productName)
-      })
-      .then(() => {
-        cy.login(adminUser.email, adminUser.password)
-        HomePage.validateLoginSuccess()
-        HomePage.accessProductList()
-        HomePage.validateProductExists(productName)
-        ShoppingCartPage.validateProductInList(productName)
-      })
+  it('Admin flow: validate product persisted via API matches the Frontend Listar Produtos', () => {
+    LoginPage.login(adminUser.email, adminUser.password)
+    HomePage.validateLoginSuccess()
+    HomePage.accessProductList()
+    HomePage.validateProductExists(productName)
   })
 })
